ce939415eb4121c31d7250d224ca6a76a7635f5b
Yaodong Li <mail@liyaodong.com>
2021-11-11 21:45:14 +0800

    Update log to show the datetime

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const http = require("http");
 const { PORT = 3000 } = process.env;
+const moment = require("moment");
 
 http
   .createServer((req, res) => {
-    res.end(`This function triggered on time ${Date.now()}`);
+    const result = `This function triggered on time ${moment().format(
+      "YYYY-MM-DD HH:mm:ss"
+    )}`;
+
+    console.log(result);
+    res.end(result);
   })
   .listen(PORT);
